765761c2a36eae8fa64fae2405b2d6b49b51763c
Douglas Fernandes Pereira da Silva Jr <douglas.pereira@iteris.com.br>
2019-05-28 15:29:44 -0300

    Criando migration

--- AcessoDados/AcessoDados.DAL/Migrations/20190528181408_CorrigirColuna.Designer.cs
+++ AcessoDados/AcessoDados.DAL/Migrations/20190528181408_CorrigirColuna.Designer.cs
@@ -4,14 +4,16 @@ using AcessoDados.DAL.EntityCodeFirst;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AcessoDados.DAL.Migrations
 {
     [DbContext(typeof(CodeFirstDBContext))]
-    partial class CodeFirstDBContextModelSnapshot : ModelSnapshot
+    [Migration("20190528181408_CorrigirColuna")]
+    partial class CorrigirColuna
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
