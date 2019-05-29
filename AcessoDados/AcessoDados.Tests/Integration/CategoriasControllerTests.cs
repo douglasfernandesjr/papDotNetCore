0d0378646416e1a3e873f384b74a76de95105da5
Douglas Fernandes Pereira da Silva Jr <douglas.pereira@iteris.com.br>
2019-05-29 17:09:39 -0300

    Adicionar testes de integração.

---
+++ AcessoDados/AcessoDados.Tests/Integration/CategoriasControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AcessoDados.Tests.Integration
+{
+	public class CategoriasControllerTests : IClassFixture<WebApplicationFactory<API.Startup>>
+	{
+		private readonly WebApplicationFactory<API.Startup> _factory;
+
+		public CategoriasControllerTests(WebApplicationFactory<API.Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Fact]
+		public async Task Get_Categorias()
+		{
+			var client = _factory.CreateClient();
+
+			var response = await client.GetAsync("/api/categorias");
+
+			Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+			string responseString = await response.Content.ReadAsStringAsync();
+
+			Assert.True(responseString.Length > 0);
+		}
+	}
+}
