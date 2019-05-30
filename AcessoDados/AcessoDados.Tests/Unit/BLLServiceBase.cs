c20f466d59a0ed1d31fe0cc63b02d7a584ae9ea6
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-05-29 23:37:19 -0300

    Adicionar novos testes

---
+++ AcessoDados/AcessoDados.Tests/Unit/BLLServiceBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace AcessoDados.Tests.Unit
+{
+	public abstract class BLLServiceTestBase
+	{
+		public IPrincipal GetMockUser()
+		{
+			return new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+			{
+				new Claim(ClaimTypes.Name, "MockUser"),
+				new Claim(ClaimTypes.NameIdentifier, "1"),
+				new Claim("custom-claim", "example claim value"),
+			}, "mock"));
+		}
+	}
+}
