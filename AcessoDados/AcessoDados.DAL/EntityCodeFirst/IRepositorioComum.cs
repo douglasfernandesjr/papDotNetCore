7ef8cbef472dfdb865a4f053502ba201714448c3
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2020-07-27 15:28:44 -0300

    Criar interfaces

---
+++ AcessoDados/AcessoDados.DAL/EntityCodeFirst/IRepositorioComum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcessoDados.DAL.EntityCodeFirst
+{
+	public interface IRepositorioComum<T>
+	{
+		 T Inserir(T modelo);
+
+
+		 List<T> Listar();
+
+		void Excluir(T modelo);
+
+		 T Atualizar(T modelo);
+	}
+}
---
+++ AcessoDados/AcessoDados.DAL/EntityCodeFirst/IRepositorioVideos.cs
@@ -0,0 +1,17 @@
+﻿using AcessoDados.DAL.EntityCodeFirst.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcessoDados.DAL.EntityCodeFirst
+{
+	interface IRepositorioVideos : IRepositorioComum<Video>
+	{
+
+		Video InserirVideo(Video modelo, int[] idCategorias);
+
+		Video ObterVideoCompleto(int id);
+
+		List<Video> ListarVideosCompletos();
+	}
+}
